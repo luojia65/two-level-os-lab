3d3c06d75d03bab9ff86f8b1543b47d6c5050efd
luojia65 <me@luojia.cc>
2020-07-14 23:48:48 +0800

    Small fix

--- machine-app-qemu/src/main.rs
+++ machine-app-qemu/src/main.rs
@@ -38,25 +38,27 @@ fn main() -> ! {
         static mut _sheap: u8;
         static _heap_size: u8;
     }
-    let sheap = unsafe { &mut _sheap } as *mut _ as usize;
-    let heap_size = unsafe { &_heap_size } as *const u8 as usize;
-    unsafe {
-        ALLOCATOR.lock().init(sheap, heap_size);
-    }
-
-    let serial = hal::Ns16550a::new(0x10000000, 0, 11_059_200, 115200);
-
-    unsafe { init_legacy_stdio_embedded_hal(serial); }
-
-    println!(
+    let mhartid = riscv::register::mhartid::read();
+    if mhartid == 0 {
+        let sheap = unsafe { &mut _sheap } as *mut _ as usize;
+        let heap_size = unsafe { &_heap_size } as *const u8 as usize;
+        unsafe {
+            ALLOCATOR.lock().init(sheap, heap_size);
+        }
+    
+        let serial = hal::Ns16550a::new(0x10000000, 0, 11_059_200, 115200);
+    
+        unsafe { init_legacy_stdio_embedded_hal(serial); }
+        println!(
 r#".______       __    __       _______.___________.  _______..______    __  
 |   _  \     |  |  |  |     /       |           | /       ||   _  \  |  | 
 |  |_)  |    |  |  |  |    |   (----`---|  |----`|   (----`|  |_)  | |  | 
 |      /     |  |  |  |     \   \       |  |      \   \    |   _  <  |  | 
 |  |\  \----.|  `--'  | .----)   |      |  |  .----)   |   |  |_)  | |  | 
-| _| `._____| \______/  |_______/       |__|  |_______/    |______/  |__| 
-
-rust_sbi v0.1.0 "#);
+| _| `._____| \______/  |_______/       |__|  |_______/    |______/  |__| "#);
+        println!("[rustsbi] Version 0.1.0");
+        println!("[rustsbi] Kernel entry: 0x80200000");
+    } // else wait for software interrupt
     
     unsafe { llvm_asm!("
     .option push
--- supervisor-app/src/main.rs
+++ supervisor-app/src/main.rs
@@ -9,6 +9,8 @@ static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
 const HEAP_SIZE: usize = 0x100_0000;
 static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 
+use riscv_sbi::println;
+
 #[cfg(target_pointer_width = "64")]
 riscv_sbi_rt::boot_page_sv39! {
     (0xffffffff_80000000 => 0x00000000_80000000, rwx);
@@ -23,5 +25,6 @@ fn main(hartid: usize, dtb_pa: usize) {
             .lock()
             .init(HEAP.as_ptr() as usize, HEAP_SIZE);
     }
-    riscv_sbi::println!("操作系统启动！")
+    println!("[Kernel] hartid: {}, dtb_pa: 0x{:x}", hartid, dtb_pa);
+    println!("操作系统启动！")
 }
