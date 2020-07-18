59468b97d7b675c9bb52aab5e9ddb9ef661ec49a
luojia65 <me@luojia.cc>
2020-07-18 23:36:57 +0800

    Save

--- machine-firmware-k210/build.rs
+++ machine-firmware-k210/build.rs
@@ -1,18 +1,13 @@
-use std::env;
-use std::fs;
-use std::io::Write;
+use std::{env, fs};
 use std::path::PathBuf;
+use std::io::Write;
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    // Put the linker script somewhere the linker can find it
-    fs::File::create(out_dir.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
     println!("cargo:rustc-link-search={}", out_dir.display());
 
-    println!("cargo:rerun-if-changed=build.rs");
+    fs::File::create(out_dir.join("memory.x")).unwrap()
+        .write_all(include_bytes!("memory.x")).unwrap();
     println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=build.rs");
 }
--- machine-firmware-k210/memory.x
+++ machine-firmware-k210/memory.x
@@ -1,12 +1,4 @@
-MEMORY {
-    /* 存储单元的物理地址 */
-    SRAM : ORIGIN = 0x80000000, LENGTH = 2M
-}
-
-PROVIDE(_stext = 0x80000000);
-PROVIDE(_heap_size = 128K);
-PROVIDE(_hart_stack_size = 64K);
-PROVIDE(_max_hart_id = 7); /* todo */
+INCLUDE memory-k210.x
 
 REGION_ALIAS("REGION_TEXT", SRAM);
 REGION_ALIAS("REGION_RODATA", SRAM);
@@ -14,3 +6,7 @@ REGION_ALIAS("REGION_DATA", SRAM);
 REGION_ALIAS("REGION_BSS", SRAM);
 REGION_ALIAS("REGION_HEAP", SRAM);
 REGION_ALIAS("REGION_STACK", SRAM);
+
+PROVIDE(_heap_size = 128K);
+PROVIDE(_hart_stack_size = 64K);
+PROVIDE(_max_hart_id = 1);
